2d701b192f5d414b6939c4292f3ab39aa1e59731
sunrha <admin@sunrha>
2015-09-11 07:37:58 -0700

    first working version

--- javascript/magictrick.js
+++ javascript/magictrick.js
@@ -1,11 +1,10 @@
 
-
 // Card Data
 //
 var suits = ['C','H','S','D'];
 var ranks = [2,3,4,5,6,7,8,9,10,'J','Q','K','Ace'];
 var print = function(a) { console.log(a); }
-
+var TOP = 0, MIDDLE = 1, BOTTOM = 2;
 
 
 function createDeck() {
@@ -43,9 +42,6 @@ function shuffle(array) {
 }
 
 
-function shuffleDeck() {
-  shuffledDeck = shuffleDeck(deck);
-}
 
 
 function getCard(colNum, pos) {
@@ -91,6 +87,16 @@ var cardChosen = false;
 var favoriteNumberSelected = false;
 var HUD = document.getElementById("HUD");
 var okBtn = document.getElementById("okBtn");
+var favoriteNumber;
+
+// 3 member array that specifies which order the
+// users card shoul be placed
+
+var columnStackOrder = [];
+
+
+//track the times user selected columns, 3 max
+var columnChooseCount = 0;
 
 /*
 var btnOK = document.createElement("Button");
@@ -101,20 +107,15 @@ btnOK.appendChild(document.createTextNode("OK"));
 
 
 
-function onCardChosen() {
-  //after card is chosen chose favorite number
-  cardChosen = true;
-  console.log("card chosen now running select favorite number");
-  STATErunSelectFavoriteNumber();
-
-}
-
+// ----------------------------
+// STATES
 
 function STATErunCardSelection() {
+  console.log("STATE run card selection");
   var string = "Choose a card and remember it... ";
   var txt = document.createTextNode(string);
 
-  console.log("STATE run card selection");
+  okBtn.style.visibility = "visible";
 
   if(okBtn) {
     okBtn.onclick = onCardChosen;
@@ -144,7 +145,6 @@ function STATErunSelectFavoriteNumber(){
     inputNumber.min = 1;
     inputNumber.max = 27;
 
-    console.log("got here 2");
     setInstructions(string);
     HUD.insertBefore(inputNumber, HUD.getElementsByClassName("instruction")[0].nextSibling);
 
@@ -152,55 +152,351 @@ function STATErunSelectFavoriteNumber(){
 }
 
 
-function setInstructions(msg) {
-  var txt = document.createTextNode(msg);
-  HUD.getElementsByClassName("instruction")[0].innerHTML = '';
-  HUD.getElementsByClassName("instruction")[0].appendChild(txt);
+
+function initSTATEchooseColumn() {
+  //set click handlers for the buttons
+  var btns = document.getElementsByClassName("columnSelectBtn");
+
+  for(var i=0; i < btns.length; ++i) {
+    btns[i].onclick = createFunction(i);
+  }
+
+  STATEchooseColumn();
 }
 
 
-function onFavoriteNumberSelected() {
-  print("on favorite number selected handler");
-  var favoriteNumberSelected = false;
+function STATEchooseColumn() {
 
-  //get rid of OK button
-  //replace instructions with the choosen number
-  setInstructions("Your chosen number is: " + document.getElementById("numberChoice").value);
-  document.getElementById("numberChoice").style.visibility = "hidden";
-  okBtn.style.visibility = "hidden";
+  //turn on the buttons
+  var btns = document.getElementsByClassName("columnSelectBtn");
 
-}
-
-function reset() {
-  cardChosen = false;
-  favoriteNumberSelected = false;
+  for(var i=0; i < btns.length; ++i) {
+    btns[i].style.visibility = "visible";
+  }
 }
 
 
 // ----------------------------
 
-reset();
-STATErunCardSelection();
 
+function setInstructions(msg) {
+  HUD.getElementsByClassName("instruction")[0].innerHTML = '';
+  appendInstructions(msg);
+}
+
+function appendInstructions(msg) {
+  var txt = document.createTextNode(msg);
+  HUD.getElementsByClassName("instruction")[0].appendChild(txt);
+}
+
+// -----------------------
+// handlers
+// -----------------------
+
+function onSelectedColumn(i) {
+  console.log("you selected column:: " + i);
+
+
+  if(columnChooseCount < 2) {
+    console.log("column " + i + " should be stacked on the: " + columnStackOrder[columnChooseCount]);
+
+    //redeal the cards based on the column
+    trickDeck = stackDeck(trickDeck,i,columnStackOrder[columnChooseCount]);
+    dealCards(trickDeck);
+    //hide the buttons
+    //show the buttons
+    //add to the instructions
+    appendInstructions(" Now which column is your card in? ");
+    columnChooseCount++;
+  } else {
+    trickDeck = stackDeck(trickDeck,i,columnStackOrder[columnChooseCount]);
+    dealCards(trickDeck);
+   console.log("WE SHOULD NOW CHOOSE THE CARD -- transition state");
+    console.log("Your card is: " + trickDeck[favoriteNumber-1] );
+
+    foundCard = getCardHTML(favoriteNumber-1);
+    foundCard.style.background = "#FF0";
+
+  }
+}
+
+function onFavoriteNumberSelected() {
+
+  var favoriteNumberSelected = false;
+
+  //get rid of OK button
+  //replace instructions with the choosen number
+  favoriteNumber = document.getElementById("numberChoice").value;
+  setInstructions("Your chosen number is: " + favoriteNumber);
+  document.getElementById("numberChoice").style.visibility = "hidden";
+  okBtn.style.visibility = "hidden";
+
+  setColumnStackOrder();
+  initSTATEchooseColumn();
+
+}
+
+
+function onCardChosen() {
+  //after card is chosen chose favorite number
+  cardChosen = true;
+  console.log("card chosen now running select favorite number");
+  STATErunSelectFavoriteNumber();
+
+}
+
+
+// use this to create an external variable
+//reference for the loop variable.
+// see here for explanation:
+// http://stackoverflow.com/questions/750486/javascript-closure-inside-loops-simple-practical-example
+function createFunction(i) {
+  return function() { onSelectedColumn(i); }
+}
+
+
+
+// ---------------
+
+function stackDeck(deck,selColumn, stackPos) {
+  var stack1 = [], stack2 = [], stack3 = [];
+  var selectedStack;
+  var newDeck = [];
+
+  for(var i=1; i <= 27; i++) {
+    var ind = i-1;
+    if(ind < 0 || ind >= deck.length) { break; }
+
+    switch(i%3) {
+      case 0:
+        stack3.push(deck[ind]);
+        break;
+      case 1:
+        stack1.push(deck[ind]);
+        break;
+      case 2:
+        stack2.push(deck[ind]);
+        break;
+    }
+  }
+
+  // --- TESTING - Display stacks
+  //debug testing deck and layout
+  console.log("---_Stacks-----");
+  printDeck(stack1);
+  printDeck(stack2);
+  printDeck(stack3);
+  console.log("---_____-----");
+  // ---
+
+  var stacks = [stack1,stack2,stack3];
+
+  switch(stackPos) {
+    case TOP:
+      if( selColumn == 0 ) {
+//        console.log("--DEBUG--stack choice 0");
+        newDeck = stacks[0].concat(stacks[1]).concat(stacks[2]);
+      } else if( selColumn == 1 ) {
+//        console.log("--DEBUG--stack choice 1");
+        newDeck = stacks[1].concat(stacks[0]).concat(stacks[2]);
+      } else if( selColumn == 2) {
+//        console.log("--DEBUG--stack choice 2");
+        newDeck = stacks[2].concat(stacks[0]).concat(stacks[1]);
+      }
+      break;
+    case MIDDLE:
+      if( selColumn == 0 ) {
+//        console.log("--DEBUG--stack choice 3");
+        newDeck = stacks[1].concat(stacks[0]).concat(stacks[2]);
+      } else if( selColumn == 1 ) {
+//        console.log("--DEBUG--stack choice 4");
+        newDeck = stacks[0].concat(stacks[1]).concat(stacks[2]);
+      } else if( selColumn == 2) {
+//        console.log("--DEBUG--stack choice 5");
+        newDeck = stacks[0].concat(stacks[2]).concat(stacks[1]);
+      }
+      break;
+    case BOTTOM:
+      if( selColumn == 0 ) {
+//        console.log("--DEBUG--stack choice 6");
+        newDeck = stacks[1].concat(stacks[2]).concat(stacks[0]);
+      } else if( selColumn == 1 ) {
+//        console.log("--DEBUG--stack choice 7");
+        newDeck = stacks[0].concat(stacks[2]).concat(stacks[1]);
+      } else if( selColumn == 2) {
+//        console.log("--DEBUG--stack choice 8");
+        newDeck = stacks[0].concat(stacks[1]).concat(stacks[2]);
+      }
+      break;
+  }
+  return newDeck;
+
+}
+
+
+function getCardHTML(deckInd) {
+  var col;
+  var row = Math.ceil( (deckInd+1) / 3) ;
+
+  var mod3 = (deckInd+1) % 3;
+  switch (mod3) {
+    case 0:
+      col = "column3";
+      break;
+    case 1:
+      col = "column1";
+      break;
+    case 2:
+      col = "column2";
+      break;
+  }
+
+  var colHTML = document.getElementById(col).getElementsByClassName("card")[row-1];
+//  console.log(colHTML.innerHTML);
+//  console.log(colHTML.getElementsByClassName("rank") +"" + colHTML.getElementsByClassName("suit"));
+  return colHTML;
+}
+
+
+function setColumnStackOrder() {
+  var mod3 = favoriteNumber % 3;
+
+  switch (mod3) {
+    case 0:
+      columnStackOrder[0] = BOTTOM;
+      break;
+    case 1:
+      columnStackOrder[0] = TOP;
+      break;
+    case 2:
+      columnStackOrder[0] = MIDDLE;
+      break;
+  }
+
+  var int3 = Math.ceil(favoriteNumber / 3) ;
+  int3 = int3 % 3;
+
+  switch (int3) {
+    case 0:
+      columnStackOrder[1] = BOTTOM;
+      break;
+    case 1:
+      columnStackOrder[1] = TOP;
+      break;
+    case 2:
+      columnStackOrder[1] = MIDDLE;
+      break;
+  }
+
+  var int9 = Math.ceil(favoriteNumber / 9);
+
+
+  switch (int9) {
+    case 1:
+      columnStackOrder[2] = TOP;
+      break;
+    case 2:
+      columnStackOrder[2] = MIDDLE;
+      break;
+    case 3:
+      columnStackOrder[2] = BOTTOM;
+      break;
+  }
+
+  console.log("column Stack Order: " + columnStackOrder);
+}
+
+
+function reset() {
+  console.log("reset");
+  cardChosen = false;
+  favoriteNumberSelected = false;
+  columnChooseCount = 0;
+
+  setupBoard();
+}
+
+
+function setupBoard() {
+  deck = createDeck();
+  shuffledDeck = shuffle(deck);
+  trickDeck = []
+
+  for(var i=0; i < 27; i++) {
+   trickDeck[i] = shuffledDeck[i];
+  }
+
+  dealCards(trickDeck);
+}
+
+
+function dealCards(aDeck) {
+  //set columnCard
+  console.log("dealing cards...");
+  var row = 0;
+  for(var i=0; i < 25; i += 3) {
+    var card1 = getCardRankSuit(aDeck[i]);
+    var card2 = getCardRankSuit(aDeck[i+1]);
+    var card3 = getCardRankSuit(aDeck[i+2]);
+
+
+    setCard(0, row, card1.r, card1.s );
+    setCard(1, row, card2.r, card2.s );
+    setCard(2, row, card3.r, card3.s );
+
+    row++;
+  }
+
+  //debug testing deck and layout
+  printDeck(aDeck);
+
+}
+
+function getCardRankSuit(aCard) {
+  var ret = {};
+  ret.r = aCard.substr(0,aCard.length - 1);
+  ret.s = aCard.substr(-1,1);
+
+  return ret;
+}
+
+
+function printDeck(aDeck) {
+
+  //debug testing deck and layout
+  var s = "";
+  for(i = 0; i< aDeck.length; i++) {
+    s += getCardRankSuit(aDeck[i]).r;
+    s += getCardRankSuit(aDeck[i]).s;
+    s += ",";
+  }
+  console.log(aDeck.length + "   " + s);
+  return s;
+
+}
+
+// ----------------------------
+
+var deck; //= createDeck();
+var shuffledDeck; // = shuffle(deck);
+var trickDeck; // = []
+
+
+reset();
+
+STATErunCardSelection();
 
 /*
-STATErunCardSelection();
-print(okBtn.onclick);
+Testing
 
-newDeck = createDeck();
-print(newDeck.length);
-for( var i=0; i < deck.length; ++i) {
-  print(newDeck[i]);
-}
+print(trickDeck.length);
+printDeck(trickDeck);
+test = stackDeck(0,MIDDLE);
+printDeck(test);
 
-var newShuffledDeck = shuffle(newDeck);
+test = stackDeck(0,BOTTOM);
+printDeck(test);
 
-for(var i=0; i < 10; i++) {
-  print(newShuffledDeck[i]);
-}
-
-
-setCard(0,1, "Me", "you");
-
-print(document.getElementsByClassName("column")[0].getElementsByClassName("card")[0].getElementsByClassName("suit")[0].className);
+favoriteNumber;
+columnStackOrder;
 */
